7806cd70ffdfacdb6b28c3aa1b8c4d5ae9000217
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-26 18:23:09 +0530

    feat(http/ws-server): refactor JWT_SECRET handling and add validation schemas for user and room operations

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,23 +1,44 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   //db call
+
   console.log(req.body);
   res.json({ userId: "123" });
 });
 
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
   res.json({ token });
 });
 
 app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   //db call
 
   console.log(req.body);
