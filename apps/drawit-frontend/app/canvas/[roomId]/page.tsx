387ca906de7b6c536194670a031485889fec1b34
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-29 00:52:18 +0530

    feat(drawit/frontend): enhance canvas drawing functionality and adjust canvas size

--- apps/drawit-frontend/app/canvas/[roomId]/page.tsx
+++ apps/drawit-frontend/app/canvas/[roomId]/page.tsx
@@ -11,8 +11,12 @@ export default function Canvas() {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
 
-      if (!ctx) return;
+      if (!ctx) {
+        return;
+      }
 
+      ctx.fillStyle = "rgba(0, 0, 0)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       //the dumb approach but works
 
       let clicked = false;
@@ -24,15 +28,18 @@ export default function Canvas() {
         startX = e.clientX;
         startY = e.clientY;
       });
+
       canvas.addEventListener("mouseup", (e) => {
         clicked = false;
         console.log(e.clientX, e.clientY);
       });
+
       canvas.addEventListener("mousemove", (e) => {
         if (clicked) {
           const width = e.clientX - startX;
           const height = e.clientY - startY;
           ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.strokeStyle = "rgba(255, 255, 255)";
           ctx.strokeRect(startX, startY, width, height);
         }
       });
@@ -41,7 +48,7 @@ export default function Canvas() {
 
   return (
     <div>
-      <canvas ref={canvasRef} width={800} height={600}></canvas>
+      <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
   );
 }
