2a40ba57758d9c95d0a184872a6be82913afa435
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-29 13:26:07 +0530

    feat(drawit/frontend): add shape management to canvas drawing with rectangle support

--- apps/drawit-frontend/draw/index.ts
+++ apps/drawit-frontend/draw/index.ts
@@ -1,5 +1,22 @@
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      x: number;
+      y: number;
+      radius: number;
+    };
+
 export function initDraw(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
+
+  let existingShapes: Shape[] = [];
 
   if (!ctx) {
     return;
@@ -21,18 +38,41 @@ export function initDraw(canvas: HTMLCanvasElement) {
 
   canvas.addEventListener("mouseup", (e) => {
     clicked = false;
-    console.log(e.clientX, e.clientY);
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      width,
+      height,
+    });
   });
 
   canvas.addEventListener("mousemove", (e) => {
     if (clicked) {
       const width = e.clientX - startX;
       const height = e.clientY - startY;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(0, 0, 0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      clearCanvas(existingShapes, canvas, ctx);
       ctx.strokeStyle = "rgba(255, 255, 255)";
       ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
+
+function clearCanvas(
+  existingShapes: Shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0, 0, 0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255, 255, 255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
     }
   });
 }
