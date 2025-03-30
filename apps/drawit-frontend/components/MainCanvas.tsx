ab16653c799f036861a5396fa45f256e67d54403
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-30 14:21:23 +0530

    feat(drawit/frontend): add tool selection and enhance shape drawing functionality

--- apps/drawit-frontend/components/MainCanvas.tsx
+++ apps/drawit-frontend/components/MainCanvas.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useEffect } from "react";
 import { initDraw } from "@/draw";
+import { Tool, Topbar } from "./TopBar";
 
 export function MainCanvas({
   roomId,
@@ -11,16 +12,33 @@ export function MainCanvas({
   socket: WebSocket;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [selectedTool, setSelectedTool] = useState<Tool>("circle");
+
+  useEffect(() => {
+    //@ts-ignore
+
+    window.selectedTool = selectedTool;
+  }, [selectedTool]);
 
   useEffect(() => {
     if (canvasRef.current) {
       initDraw(canvasRef.current, roomId, socket);
     }
-  }, [canvasRef, roomId]);
+  }, [canvasRef, roomId, socket]);
 
   return (
-    <div>
-      <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+    <div
+      style={{
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      ></canvas>
+      <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
     </div>
   );
 }
